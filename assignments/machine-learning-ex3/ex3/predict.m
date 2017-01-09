8f659bccb98f2e95380f3d63a1000be9888872ae
chorro73 <chorro73@gmail.com>
2017-01-09 15:06:48 -0800

    Neural Network working

--- assignments/machine-learning-ex3/ex3/predict.m
+++ assignments/machine-learning-ex3/ex3/predict.m
@@ -20,14 +20,13 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+a1 = [ones(size(X, 1), 1), X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1), a2];
+a3 = sigmoid(a2 * Theta2');
 
-
-
-
-
-
-
-
+[unused, p] = max(a3, [], 2);
 
 % =========================================================================
 
