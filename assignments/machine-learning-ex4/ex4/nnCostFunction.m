2e89b09c22626b7f2a82b7957a015e000c851ecb
chorro73 <chorro73@gmail.com>
2017-01-13 13:24:07 -0800

    Finish first draft of getting a3.

--- assignments/machine-learning-ex4/ex4/nnCostFunction.m
+++ assignments/machine-learning-ex4/ex4/nnCostFunction.m
@@ -61,20 +61,13 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+y_matrix = eye(num_labels)(y,:);
+a1 = [ones(size(a1, 1), 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
 
 
